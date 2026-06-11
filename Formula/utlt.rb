865b545efd6468ef187761d0203c0d1710076821
box1a <box1a@users.noreply.github.com>
2026-06-11 11:08:58 -0400

    release: publish utlt 0.1.8

--- Formula/utlt.rb
+++ Formula/utlt.rb
@@ -1,20 +1,20 @@
 class Utlt < Formula
   desc "Installer and launcher for Arendi products"
   homepage "https://github.com/ArendiStudio/homebrew-apps"
-  version "0.1.7"
+  version "0.1.8"
   license :cannot_represent
 
   if OS.mac?
     url "https://raw.githubusercontent.com/ArendiStudio/homebrew-apps/main/artifacts/utlt/#{version}/aarch64-apple-darwin/utlt-#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "c8f8766977b5d8726774a1be2b513820317193425075c009e61c546cb68c4a75"
+    sha256 "f696aad1dfa159e34b6c37ac7f8b450ec400c5f06d0d6d38f12aaa449e2f3a0f"
 
     depends_on arch: :arm64
   elsif Hardware::CPU.arm?
     url "https://raw.githubusercontent.com/ArendiStudio/homebrew-apps/main/artifacts/utlt/#{version}/aarch64-unknown-linux-gnu/utlt-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "6b912c1f8418129ee55de0cdf0810965391e0c5f26f14034469ffe3d54b1af6b"
+    sha256 "576dc579a10213ceb8cff335454db11a307a8e49c650ca97967d882a7d5924b7"
   else
     url "https://raw.githubusercontent.com/ArendiStudio/homebrew-apps/main/artifacts/utlt/#{version}/x86_64-unknown-linux-gnu/utlt-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "2ec68f530a0c08d03d3bbb353c734ed5984888aedeb7e7fce75386b0729988ac"
+    sha256 "2e4589593702431150cafc315f16921a1ced9310e0ac00a4698aa8d969492d09"
   end
 
   def install
